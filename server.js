b13cb6b200b96321c6b73d0b9d9a5744bc4dcdba
kevinadhiguna <kevin26798@gmail.com>
2020-10-25 23:04:12 +0700

    build: release v0.1

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+const Article = require('./models/article');
+const articleRouter = require('./routes/articles');
+
+const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' });
+    res.render('articles/index', { articles: articles });
+});
+
+app.use('/articles', articleRouter);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('🚀 Server is running at port ' + PORT));

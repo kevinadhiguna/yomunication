cde3ad62471943f5a5bff8005463be642aa755ca
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2022-04-16 09:57:36 +0000

    refactor: compress response bodies

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const compression = require("compression");
 const createHttpTerminator = require("lil-http-terminator");
 
 const Article = require("./models/article");
 const articleRouter = require("./routes/articles");
 
 const app = express();
+
+app.use(compression());
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
